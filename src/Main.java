57e4aa4a973badaa4ccdcd6becefb5fa69846a63
sasa12366 <54961843+sasa12366@users.noreply.github.com>
2026-02-09 18:47:19 +0300

    add first version

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int a =input.nextInt();
+        float b= input.nextFloat();
+        double c=a+b;
+        System.out.println(c);
+    }
+}
